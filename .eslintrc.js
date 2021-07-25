2940b79035e6bc233cea95206a2fa264a4974df0
Yudai Nakajima <kituneudon97@gmail.com>
2021-07-25 14:48:34 +0900

    prettierを導入し、eslintのエラーを解消

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
