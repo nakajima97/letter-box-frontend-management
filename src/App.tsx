fdb1b201e8e0c41a9bfc93d0a8440718190e6eff
Yudai Nakajima <kituneudon97@gmail.com>
2021-07-28 12:29:32 +0900

    #1 react-router-domを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LoginPage from './components/pages/LoginPage';
+import StoreManagementPage from './components/pages/StoreManagementPage';
 
-const App: FC = () => <LoginPage />;
+const App: FC = () => (
+  <Router>
+    <Switch>
+      <Route exact path={['/', '/login']}>
+        <LoginPage />
+      </Route>
+      <Route path="/store">
+        <StoreManagementPage />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
