1e896891b8e82a55df85916b2f0ccc92c0bfd50c
Yudai Nakajima <kituneudon97@gmail.com>
2021-08-25 15:38:05 +0900

    #5 メッセージ一覧画面でヘッダーがなかったので追加

--- src/components/templates/MessageListTemplate/index.tsx
+++ src/components/templates/MessageListTemplate/index.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
 
 import MessageView from '../../organisms/MessageView';
+import Header from '../../organisms/Header';
 
-const Index: FC = () => <MessageView />;
+const Index: FC = () => (
+  <>
+    <Header />
+    <MessageView />
+  </>
+);
 
 export default Index;
