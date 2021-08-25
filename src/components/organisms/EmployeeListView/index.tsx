8e489a48e2aef4229ce7cc053d1aa44f69bdbf28
Yudai Nakajima <kituneudon97@gmail.com>
2021-08-25 15:54:01 +0900

    #5 従業員一覧ページにタイトルをつけた

--- src/components/organisms/EmployeeListView/index.tsx
+++ src/components/organisms/EmployeeListView/index.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react';
+import { Typography } from '@material-ui/core';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 import EmployeeList from '../../molecules/EmployeeList';
 
-const Index: FC = () => <EmployeeList />;
+const container = css`
+  width: 100%;
+  margin: 10px;
+`;
+
+const Index: FC = () => (
+  <div css={container}>
+    <Typography gutterBottom>従業員一覧</Typography>
+    <EmployeeList />
+  </div>
+);
 
 export default Index;
