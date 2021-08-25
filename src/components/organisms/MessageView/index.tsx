2e1c88229f8e6aec0b7cdc524cace62b4ec0ed05
Yudai Nakajima <kituneudon97@gmail.com>
2021-08-25 15:46:29 +0900

    #5 メッセージ一覧画面にタイトルを追加し、細かい調整を加えた

--- src/components/organisms/MessageView/index.tsx
+++ src/components/organisms/MessageView/index.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { Typography } from '@material-ui/core';
 
 import MessageList from '../../molecules/MessageList';
 
 const container = css`
   width: 100%;
+  margin: 10px;
 `;
 
 const Index: FC = () => (
   <div css={container}>
+    <Typography gutterBottom color="textPrimary">
+      メッセージ一覧
+    </Typography>
     <MessageList />
   </div>
 );
