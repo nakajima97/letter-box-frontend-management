dad90d97798bd45f6a35e137e1e6ed7c02192cef
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-15 15:22:36 +0900

    #8 formの値をstateで管理するにした

--- src/components/organisms/LoginForm/index.tsx
+++ src/components/organisms/LoginForm/index.tsx
@@ -1,9 +1,11 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { TextField, Button, Typography } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 import LoginTypeSelector from '../../molecules/LoginTypeSelector';
+import LoginType from '../../../types/loginTypes';
 
 const container = css`
   display: grid;
@@ -15,36 +17,53 @@ const title = css`
   text-align: center;
 `;
 
-const Index: FC = () => (
-  <>
-    <Typography component="h1" variant="h5" css={title}>
-      Log In
-    </Typography>
-    <form css={container}>
-      <div>
-        <LoginTypeSelector />
-      </div>
-      <TextField
-        required
-        fullWidth
-        label="ID"
-        variant="outlined"
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        required
-        label="Password"
-        variant="outlined"
-        margin="normal"
-      />
-      <div>
-        <Button variant="contained" color="primary" fullWidth>
-          ログイン
-        </Button>
-      </div>
-    </form>
-  </>
-);
+const Index: FC = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginType, setLoginType] = useState<LoginType>('store');
+  const history = useHistory();
+
+  const loginHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    history.push('/employee');
+  };
+
+  return (
+    <>
+      <Typography component="h1" variant="h5" css={title}>
+        Log In
+      </Typography>
+      <form css={container} onSubmit={loginHandler}>
+        <div>
+          <LoginTypeSelector value={loginType} onChange={setLoginType} />
+        </div>
+        <TextField
+          required
+          fullWidth
+          label="ID"
+          variant="outlined"
+          margin="normal"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <TextField
+          fullWidth
+          required
+          label="Password"
+          variant="outlined"
+          margin="normal"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div>
+          <Button variant="contained" color="primary" fullWidth>
+            ログイン
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default Index;
