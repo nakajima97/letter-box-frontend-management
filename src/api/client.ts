e4abba0fd2e8974de851d0cded4503a3fea064b6
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-19 14:10:14 +0900

    #27 キャメルケースとスネークケースを相互変換するパッケージを導入

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 import Cookie from 'js-cookie';
+import applyCaseMiddleware from 'axios-case-converter';
 
 const createHeader = (jwt: string | undefined) => {
   if (jwt) {
@@ -19,11 +20,13 @@ const createAxiosClient = (): AxiosInstance => {
 
   const headers = createHeader(jwt);
 
-  return axios.create({
-    headers,
-    withCredentials: true,
-    baseURL: 'http://localhost:3000/api/v1/',
-  });
+  return applyCaseMiddleware(
+    axios.create({
+      headers,
+      withCredentials: true,
+      baseURL: 'http://localhost:3000/api/v1/',
+    }),
+  );
 };
 
 export default createAxiosClient;
