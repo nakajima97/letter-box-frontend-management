4d078f27c1c1ae34c3e288d088a5fbbde3beccea
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-20 17:05:18 +0900

    #31 storyを動作するよう変更

--- src/components/molecules/EmployeeList/index.stories.tsx
+++ src/components/molecules/EmployeeList/index.stories.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import EmployeeType from '../../../types/employeeType';
 import EmployeeList from './index';
 
 export default {
@@ -6,4 +7,9 @@ export default {
   title: 'molecules/EmployeeList',
 };
 
-export const Default: FC = () => <p>改修中</p>;
+const employees: EmployeeType[] = [
+  { id: 1, firstName: '竈門', lastName: 'ピカチュウ' },
+  { id: 2, firstName: '鬼舞辻', lastName: 'ジバニャン' },
+];
+
+export const Default: FC = () => <EmployeeList employees={employees} />;
