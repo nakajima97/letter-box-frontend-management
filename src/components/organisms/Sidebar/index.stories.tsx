4d1023a5ab929c191121ef83c00efed9daf5ef68
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-30 19:09:59 +0900

    #43 storyを追加

---
+++ src/components/organisms/Sidebar/index.stories.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import SideBar from './index';
+
+export default {
+  component: SideBar,
+  title: 'organisms/SideBar',
+};
+
+export const Default: FC = () => <SideBar />;
