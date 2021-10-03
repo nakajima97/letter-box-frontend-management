9be41c123de513efaedfc82c1d7a6e54401a4435
Yudai Nakajima <kituneudon97@gmail.com>
2021-10-03 14:42:40 +0900

    #39 従業員登録するコードを作成

--- src/components/organisms/EmployeeRegistrationForm/index.tsx
+++ src/components/organisms/EmployeeRegistrationForm/index.tsx
@@ -1,6 +1,9 @@
-import { FC, useState } from 'react';
+import { FC, useState, useContext, FormEvent } from 'react';
 import { Container, TextField, Typography, Button, Grid } from '@mui/material';
 import { Box } from '@mui/system';
+
+import createAxiosClient from '../../../api/client';
+import { AuthContext } from '../../../contexts/Auth';
 
 const Index: FC = () => {
   const [firstName, setFirstName] = useState('');
@@ -8,6 +11,35 @@ const Index: FC = () => {
   const [loginId, setLoginId] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
+  const { loggedInUserId } = useContext(AuthContext);
+
+  const axiosClient = createAxiosClient();
+
+  const handleSignUp = (event: FormEvent) => {
+    const params = {
+      employee_auth: { employee_login_id: loginId, password },
+      employee: { firstName, lastName, storeId: loggedInUserId },
+    };
+
+    axiosClient
+      .post('employee', params)
+      // eslint-disable-next-line
+      .then((res) => console.log(res))
+      // eslint-disable-next-line
+      .catch((err) => console.log(err));
+
+    event.preventDefault();
+  };
+
+  const checkValue = () =>
+    !(
+      firstName &&
+      lastName &&
+      loginId &&
+      password &&
+      passwordConfirm &&
+      password === passwordConfirm
+    );
 
   return (
     <>
@@ -15,7 +47,7 @@ const Index: FC = () => {
         <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
           従業員登録
         </Typography>
-        <Box component="form" noValidate sx={{ mt: 3 }}>
+        <Box component="form" noValidate onSubmit={handleSignUp} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -65,7 +97,13 @@ const Index: FC = () => {
               />
             </Grid>
             <Grid item xs={12}>
-              <Button variant="contained" color="primary" fullWidth>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+                disabled={checkValue()}
+              >
                 登録
               </Button>
             </Grid>
