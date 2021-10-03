9511bd9de4bcea622bbf4adb77c55534b4c8cf61
Yudai Nakajima <kituneudon97@gmail.com>
2021-10-03 15:11:48 +0900

    従業員登録機能を追加 (#54)
    * #39 従業員登録ページの土台を作成
    
    * #39 従業員登録フォームの土台を作成
    
    * #39 従業員登録画面への遷移ができるようにした
    
    * #39 従業員登録フォームの静的バージョンを作成
    
    * #39 従業員登録画面にサイドバーを追加
    
    * #39 サイドバーに従業員登録画面へ遷移するためのボタンを追加
    
    * #39 従業員登録フォームのHTMLを修正
    
    * #39 テキストフィールドの値をstateで管理するよう変更
    
    * #39 従業員登録するコードを作成
    
    * #39 従業員登録処理の結果をユーザに伝える仕組みを作成
    
    * #39 従業員登録フォームのstoryを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import LoggedInRoute from './routes/LoggedInRoute';
+import NotLoggedInRoute from './routes/NotLoggedInRoute';
+import StoreLoggedInRoute from './routes/StoreLoggedInRoute';
+import EmployeeLoggedInRoute from './routes/EmployeeLoggedInRoute';
+
 import LoginPage from './components/pages/LoginPage';
 import StoreManagementPage from './components/pages/StoreManagementPage';
 import EmployeeManagementPage from './components/pages/EmployeeManagementPage';
 import MessageList from './components/pages/MessageListPage';
 import NotFound from './components/pages/NotFound';
-import LoggedInRoute from './routes/LoggedInRoute';
-import NotLoggedInRoute from './routes/NotLoggedInRoute';
-import StoreLoggedInRoute from './routes/StoreLoggedInRoute';
-import EmployeeLoggedInRoute from './routes/EmployeeLoggedInRoute';
+import EmployeeRegistration from './components/pages/EmployeeRegistration';
 
 const App: FC = () => (
   <Router>
@@ -19,6 +21,9 @@ const App: FC = () => (
       </NotLoggedInRoute>
       <StoreLoggedInRoute exact path="/store">
         <StoreManagementPage />
+      </StoreLoggedInRoute>
+      <StoreLoggedInRoute exact path="/store/employee/registration">
+        <EmployeeRegistration />
       </StoreLoggedInRoute>
       <EmployeeLoggedInRoute path="/employee">
         <EmployeeManagementPage />
---
+++ src/components/organisms/EmployeeRegistrationForm/index.tsx
@@ -0,0 +1,132 @@
+import { FC, useState, useContext, FormEvent } from 'react';
+import { Container, TextField, Typography, Button, Grid } from '@mui/material';
+import { Box } from '@mui/system';
+
+import createAxiosClient from '../../../api/client';
+import { AuthContext } from '../../../contexts/Auth';
+import { MessageContext } from '../../../contexts/Message';
+
+const Index: FC = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [loginId, setLoginId] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const { loggedInUserId } = useContext(AuthContext);
+  const { setMessage, setSeverity } = useContext(MessageContext);
+
+  const axiosClient = createAxiosClient();
+
+  const clearForm = () => {
+    setFirstName('');
+    setLastName('');
+    setLoginId('');
+    setPassword('');
+    setPasswordConfirm('');
+  };
+
+  const handleSignUp = (event: FormEvent) => {
+    const params = {
+      employee_auth: { employee_login_id: loginId, password },
+      employee: { firstName, lastName, storeId: loggedInUserId },
+    };
+
+    axiosClient
+      .post('employee', params)
+      .then(() => {
+        setMessage('従業員を登録しました。');
+        setSeverity('success');
+        clearForm();
+      })
+      .catch(() => {
+        setMessage('従業員登録に失敗しました。');
+        setSeverity('error');
+      });
+
+    event.preventDefault();
+  };
+
+  const checkValue = () =>
+    !(
+      firstName &&
+      lastName &&
+      loginId &&
+      password &&
+      passwordConfirm &&
+      password === passwordConfirm
+    );
+
+  return (
+    <>
+      <Container sx={{ paddingTop: '10px' }}>
+        <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
+          従業員登録
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSignUp} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                label="名字"
+                fullWidth
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                label="名前"
+                fullWidth
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label="ログインID"
+                value={loginId}
+                onChange={(e) => setLoginId(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label="パスワード"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label="パスワード再入力"
+                type="password"
+                value={passwordConfirm}
+                onChange={(e) => setPasswordConfirm(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+                disabled={checkValue()}
+              >
+                登録
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Index;
