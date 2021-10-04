04ee13e788397b6a47d9757f7b5b59ca477331d2
Yudai Nakajima <kituneudon97@gmail.com>
2021-10-04 18:14:33 +0900

    #48 loading中の際に表示するコンポーネントにstoryを追加

---
+++ src/components/molecules/Loading/index.stories.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+import Loading from './index';
+
+export default {
+  component: Loading,
+  title: 'molecules/Loading',
+};
+
+export const Default: FC = () => <Loading />;
